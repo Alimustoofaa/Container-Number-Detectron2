bc22ce5cc4ed4928cd7c0620d71e2fc31dfb411b
Alimustoofaa <softhouse.ali@gmail.com>
2020-11-11 12:47:31 +0700

    add error handling

--- process/checkDigit.py
+++ process/checkDigit.py
@@ -2,10 +2,13 @@ from stdnum import iso6346
 import re
 
 def checkDigitNum(text):
-    number =  text
-    num = number[0:-1]
-    dig = number[-1]
-    # csqu3054383
-    test1 = iso6346.calc_check_digit(num)
-    
-    return 'valid' if test1 == dig else 'not valid'
+    try:
+        number =  text
+        num = number[0:-1]
+        dig = number[-1]
+        # csqu3054383
+        test1 = iso6346.calc_check_digit(num)
+        
+        return 'valid' if test1 == dig else 'not valid'
+    except TypeError:
+        return 'not valid'
