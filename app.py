fe78b509a360d83e87e672577b311990fad409e0
Alimustoofaa <softhouse.ali@gmail.com>
2020-11-13 21:45:04 +0700

    fix bug

--- app.py
+++ app.py
@@ -22,57 +22,55 @@ except OSError as error:
 scalePercent = int(config.get('Resize Image', 'scalePercent'))
 minContrast  = int(config.get('Min Contrast', 'minContrast'))
 
+def getDictResult(posCam, imageName, conNum=None, strDist=None, conLevel=None, checkDig=None):
+	dictResult = {
+			'Position cam' : posCam,
+			'Image name' : imageName,
+			'Container number': conNum,
+			'String distance': strDist,
+			'Confidence level': conLevel,
+			'Check digit': checkDig
+		}
+	return dictResult
+
 def processing(image, imageName, posCam):
 	resultContainerNumber = {}
 	result = predicContainerNumber(image)
-	if result != '':
-		imageCropped = cropImage(result, image)
-		if imageCropped.shape[1] < scalePercent:
-			resizedImage = resizeImage(imageCropped)
-		else:
-			resizedImage = imageCropped.copy()
-		contrast = getContrast(resizedImage)
-		if contrast > minContrast:
-			preProcessedImage = processImage(imageCropped)
-			image = preProcessedImage.copy()
-		else:
-			image = resizedImage.copy()
-		
-		# Get Vertical or Horizontal image
-		height = image.shape[0]
-		width = image.shape[1]
-		if height < width:
-			resultEasyOcr, confidenceEasyOcr = ocrEasyOcr(image, rotate='horizontal')
-			getStrDist = getStringDistance(resultEasyOcr) if resultEasyOcr is not None else None
-			checked = checkDigitNum(getStrDist) if resultEasyOcr is not None else None
-			resultContainerNumber = {
-				'Position cam' : posCam,
-				'Image name' : imageName,
-				'Container number': resultEasyOcr,
-				'String distance': getStrDist,
-				'Confidence level': confidenceEasyOcr,
-				'Check digit':checked
-			}
-			# tesseractOcr
-			# resultTesseract, cofidenceTesseract = tesseractOcr(image)
-			# resultContainerNumber['tesseract'] = [resultTesseract, cofidenceTesseract]
-
-		else:
-			resultEasyOcr, confidenceEasyOcr = ocrEasyOcr(image, rotate='vertical')
-			getStrDist = getStringDistance(resultEasyOcr) if resultEasyOcr is not None else None
-			checked = checkDigitNum(resultEasyOcr) if resultEasyOcr is not None else None
-			resultContainerNumber = {
-				'Position cam' : posCam,
-				'Image name' : imageName,
-				'Container number': resultEasyOcr,
-				'String distance': getStrDist,
-				'Confidence level': confidenceEasyOcr,
-				'Check digit':checked
-			}
-			# tesseractOcr
-			# resultTesseract, cofidenceTesseract = tesseractOcr(image, psm=6)
-			# resultContainerNumber['tesseract'] = [resultTesseract, cofidenceTesseract]a
-		
-		return resultContainerNumber
+	imageCropped = cropImage(result, image) if result != '' else image.copy()
+	resizedImage = resizeImage(imageCropped) if imageCropped.shape[1] < scalePercent else imageCropped.copy()
+	contrast = getContrast(resizedImage)
+	if contrast > minContrast:
+		preProcessedImage = processImage(imageCropped)
+		image = preProcessedImage.copy()
 	else:
-		return 'No Container Number detected'
+		image = resizedImage.copy()
+		
+	# Get Vertical or Horizontal image
+	height = image.shape[0]
+	width = image.shape[1]
+	if height < width:
+		resultEasyOcr, confidenceEasyOcr = ocrEasyOcr(image, rotate='horizontal')
+		# log
+		from server import app
+		app.logger.info('OCR : '+resultEasyOcr if resultEasyOcr is not None else '-')
+		try:
+			getStrDist = getStringDistance(resultEasyOcr) if resultEasyOcr is not None or len(resultEasyOcr) == 0 else None
+		except TypeError:
+			getStrDist = None
+		checked = checkDigitNum(getStrDist) if resultEasyOcr is not None else None
+		resultContainerNumber = getDictResult(posCam, imageName, conNum=resultEasyOcr, strDist=getStrDist, conLevel=confidenceEasyOcr, checkDig=checked)
+		# tesseractOcr
+		# resultTesseract, cofidenceTesseract = tesseractOcr(image)
+		# resultContainerNumber['tesseract'] = [resultTesseract, cofidenceTesseract]
+	else:
+		resultEasyOcr, confidenceEasyOcr = ocrEasyOcr(image, rotate='vertical')
+		try:
+			getStrDist = getStringDistance(resultEasyOcr) if resultEasyOcr is not None or len(resultEasyOcr) == 0 else None
+		except TypeError:
+			getStrDist = None
+		checked = checkDigitNum(getStrDist) if getStrDist is not None else None
+		resultContainerNumber = getDictResult(posCam, imageName, conNum=resultEasyOcr, strDist=getStrDist, conLevel=confidenceEasyOcr, checkDig=checked)
+		# tesseractOcr
+		# resultTesseract, cofidenceTesseract = tesseractOcr(image, psm=6)
+		# resultContainerNumber['tesseract'] = [resultTesseract, cofidenceTesseract]a
+	return resultContainerNumber
